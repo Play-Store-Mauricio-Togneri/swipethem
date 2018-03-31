3c337b96080077109efb9d1ffd324f888537e6e3
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-03-31 18:03:39 +0200

    Removed leaderboard

--- app/src/main/java/com/mauriciotogneri/swipethem/activities/MainActivity.java
+++ app/src/main/java/com/mauriciotogneri/swipethem/activities/MainActivity.java
@@ -2,8 +2,6 @@ package com.mauriciotogneri.swipethem.activities;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender.SendIntentException;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Vibrator;
@@ -17,31 +15,17 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.games.Games;
 import com.mauriciotogneri.swipethem.R;
 import com.mauriciotogneri.swipethem.objects.Game;
 import com.mauriciotogneri.swipethem.util.Preferences;
 import com.mauriciotogneri.swipethem.util.Statistics;
 
-public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+public class MainActivity extends Activity
 {
     private Game game;
     private GLSurfaceView surfaceView;
 
-    private GoogleApiClient apiClient;
-    private boolean intentInProgress = false;
-    private boolean openingLeaderboard = false;
     private boolean helpOpened = false;
-
-    private static final int REQUEST_RESOLVE_ERROR = 1001;
-
-    private static final int ACHIEVEMENT_50 = 50;
-    private static final int ACHIEVEMENT_75 = 75;
-    private static final int ACHIEVEMENT_100 = 100;
-    private static final int ACHIEVEMENT_150 = 150;
-    private static final int ACHIEVEMENT_200 = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -62,73 +46,10 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         Preferences.initialize(this);
         Statistics.sendHitAppLaunched();
 
-        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this, this, this);
-        builder.addApi(Games.API).addScope(Games.SCOPE_GAMES);
-        this.apiClient = builder.build();
-
-        if (Preferences.isConnectedPlayGameServices())
-        {
-            this.apiClient.connect();
-        }
-
         if (Preferences.isFirstLaunch())
         {
             Preferences.setFirstLaunch();
             displayHelp();
-        }
-    }
-
-    @Override
-    public void onConnected(Bundle connectionHint)
-    {
-        Preferences.setConnectedPlayGameServices();
-
-        if (this.openingLeaderboard)
-        {
-            submitScore(Preferences.getBestScore());
-
-            showRanking();
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause)
-    {
-        this.apiClient.reconnect();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result)
-    {
-        if ((!this.intentInProgress) && result.hasResolution())
-        {
-            try
-            {
-                this.intentInProgress = true;
-                result.startResolutionForResult(this, MainActivity.REQUEST_RESOLVE_ERROR);
-            }
-            catch (SendIntentException e)
-            {
-                this.intentInProgress = false;
-                this.apiClient.connect();
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if (requestCode == MainActivity.REQUEST_RESOLVE_ERROR)
-        {
-            this.intentInProgress = false;
-
-            if (resultCode == Activity.RESULT_OK)
-            {
-                if ((!this.apiClient.isConnecting()) && (!this.apiClient.isConnected()))
-                {
-                    this.apiClient.connect();
-                }
-            }
         }
     }
 
@@ -187,8 +108,6 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
             Preferences.setBestScore(bestScore);
         }
 
-        submitScore(bestScore);
-
         final LinearLayout blockScreen = (LinearLayout) findViewById(R.id.block_screen);
         blockScreen.setVisibility(View.VISIBLE);
 
@@ -206,16 +125,6 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
             {
                 blockScreen.setVisibility(View.GONE);
                 restartGame();
-            }
-        });
-
-        ImageView leaderboard = (ImageView) blockScreen.findViewById(R.id.ranking);
-        leaderboard.setOnClickListener(new OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                showRanking();
             }
         });
 
@@ -252,49 +161,6 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         ScrollView howToPlay = (ScrollView) findViewById(R.id.how_to_play);
         howToPlay.setVisibility(View.GONE);
         this.helpOpened = false;
-    }
-
-    private void submitScore(int score)
-    {
-        if (this.apiClient.isConnected())
-        {
-            Games.Leaderboards.submitScore(this.apiClient, getString(R.string.leaderboard_high_scores), score);
-
-            if (score >= MainActivity.ACHIEVEMENT_200)
-            {
-                Games.Achievements.unlock(this.apiClient, getString(R.string.achievement_200));
-            }
-            else if (score >= MainActivity.ACHIEVEMENT_150)
-            {
-                Games.Achievements.unlock(this.apiClient, getString(R.string.achievement_150));
-            }
-            else if (score >= MainActivity.ACHIEVEMENT_100)
-            {
-                Games.Achievements.unlock(this.apiClient, getString(R.string.achievement_100));
-            }
-            else if (score >= MainActivity.ACHIEVEMENT_75)
-            {
-                Games.Achievements.unlock(this.apiClient, getString(R.string.achievement_75));
-            }
-            else if (score >= MainActivity.ACHIEVEMENT_50)
-            {
-                Games.Achievements.unlock(this.apiClient, getString(R.string.achievement_50));
-            }
-        }
-    }
-
-    private void showRanking()
-    {
-        if (this.apiClient.isConnected())
-        {
-            this.openingLeaderboard = false;
-            startActivityForResult(Games.Leaderboards.getLeaderboardIntent(this.apiClient, getString(R.string.leaderboard_high_scores)), 456);
-        }
-        else
-        {
-            this.openingLeaderboard = true;
-            this.apiClient.connect();
-        }
     }
 
     private void restartGame()
@@ -353,11 +219,6 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         if (this.game != null)
         {
             this.game.stop();
-        }
-
-        if ((this.apiClient != null) && this.apiClient.isConnected())
-        {
-            this.apiClient.disconnect();
         }
 
         super.onDestroy();
